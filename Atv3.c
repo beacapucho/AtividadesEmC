21dc41a25c30ed0fd21afb725118a0c3b8b2180d
beacapucho <bsabrinaq@gmail.com>
2024-01-12 20:35:30 -0300

    Imprime o nome na tela

---
+++ Atv3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	char nome[20];
+	printf ("\n Digite seu nome: ");
+	scanf ("%s", nome);
+	printf ("\n Bem vindo a disciplina de Algoritmos e logica de programacao II %s", nome);
+	return 0;
+}
